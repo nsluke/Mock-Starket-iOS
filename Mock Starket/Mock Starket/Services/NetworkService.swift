006cc5ef0b1ee6960904893919529bbd98941322
Luke Solomon <luke_solomon@me.com>
2018-01-15 18:27:56 -0800

    Websocket connection up and working. Finally.

---
+++ Mock Starket/Mock Starket/Services/NetworkService.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkService.swift
+//  Mock Starket
+//
+//  Created by Luke Solomon on 1/15/18.
+//  Copyright © 2018 Luke Solomon. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxStarscream
+
+struct NetworkService {
+    
+    
+    func receiveSocket () {
+        
+//        socket = WebSocket(url: URL(string: "ws://localhost:8000/ws")!)
+//        socket.connect()
+//        
+//        socket.rx.response.subscribe(onNext: { (response: WebSocketEvent) in
+//            switch response {
+//            case .connected:
+//                print("Connected")
+//                
+//                
+//            case .disconnected(let error):
+//                print("Disconnected with optional error : \(error)")
+//            case .message(let msg):
+//                print("Message : \(msg)")
+//            case .data(_):
+//                print("Data")
+//            case .pong:
+//                print("Pong")
+//            }
+//        }).disposed(by disposeBag)
+//        
+    }
+    
+    
+    
+    
+}
