f0400c679f16e22993dd68aa76831b2343f34a7e
Luke Solomon <luke_solomon@icloud.com>
2018-02-22 18:05:39 -0800

    Making a lot of changes to the login screen

--- Mock Starket/Mock Starket/Controllers/LoginViewController.swift
+++ Mock Starket/Mock Starket/Controllers/LoginViewController.swift
@@ -12,51 +12,172 @@ import Crashlytics
 
 class LoginViewController: ViewController {
     
+    //MARK: IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var createAccountButton: UIButton!
-    
-    var socket = WebSocket(url: URL(string: "ws://159.89.154.221:8000/ws")!)
+    @IBOutlet weak var loginActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var constraintContentHeight: NSLayoutConstraint!
 
+    //MARK: Variables
+    var activeTextField:UITextField!
+    var socket = WebSocket(url: URL(string: "ws://159.89.154.221:8000/ws")!)
+    var lastOffset: CGPoint!
+    var keyboardHeight: CGFloat!
     
+    
+    //MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UIApplication.shared.statusBarStyle = .lightContent
-        
-        loginButton.isUserInteractionEnabled = false
+        loginButton.backgroundColor = UIColor.red
+        self.loginActivityIndicator.isHidden = true
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
         socket.delegate = self
+        socket.disableSSLCertValidation = true
         socket.connect()
     }
     
-    @IBAction func loginButtonTapped(_ sender: Any) {
+    //MARK: IBActions
+    @IBAction func touchReceived(_ sender: Any) {
+        guard activeTextField != nil else { return }
         
-        guard let username = usernameTextField.text else {
-            return
-        }
-        
-        guard let password = passwordTextField.text else {
-            return
-        }
-        
-        sendLoginRequest(username: username, password: password)
+        activeTextField?.resignFirstResponder()
+        activeTextField = nil
     }
     
-    func sendLoginRequest(username:String, password:String) {
-        let message = "{\"action\": \"login\", \"value\": {\"username\": \"\(username)\", \"password\": \"\(password)\" }}"
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let username = usernameTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
         
-        socket.disableSSLCertValidation = true
+        let message = "{\"action\": \"login\", \"value\": {\"username\": \"\(username)\", \"password\": \"\(password)\" }}"
         print(message)
+        self.loginButton.titleLabel?.attributedText = NSAttributedString(string: "")
+        self.loginActivityIndicator.isHidden = false
+        self.loginActivityIndicator.startAnimating()
+        
         socket.write(string: message)
+    }
+    
+    //MARK: keyboard scroll code
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if keyboardHeight != nil {
+            return
+        }
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            keyboardHeight = keyboardSize.height
+            // so increase contentView's height by keyboard height
+            UIView.animate(withDuration: 0.3, animations: {
+                self.constraintContentHeight.constant += self.keyboardHeight
+            })
+            // move if keyboard hide input field
+            let distanceToBottom = self.scrollView.frame.size.height - (activeTextField?.frame.origin.y)! - (activeTextField?.frame.size.height)!
+            let collapseSpace = keyboardHeight - distanceToBottom
+            if collapseSpace < 0 {
+                // no collapse
+                return
+            }
+            // set new offset for scroll view
+            UIView.animate(withDuration: 0.3, animations: {
+                // scroll to the position above keyboard 10 points
+                self.scrollView.contentOffset = CGPoint(x: self.lastOffset.x, y: collapseSpace + 10)
+            })
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.constraintContentHeight.constant -= self.keyboardHeight
+            self.scrollView.contentOffset = self.lastOffset
+        }
+        keyboardHeight = nil
+    }
+    
+    //MARK: textField Checks
+    func checkForErrors() -> Bool {
+        var errors = false
+        let title = "Error"
+        var message = ""
+        
+        if usernameTextField.text!.isEmpty {
+            errors = true
+            message += "First name empty"
+            alertWithTitle(title, message: message, ViewController: self, toFocus:self.usernameTextField)
+        } else if passwordTextField.text!.isEmpty {
+            errors = true
+            message += "Surname empty"
+            alertWithTitle(title, message: message, ViewController: self, toFocus:self.passwordTextField)
+            
+        } else if !isValidEmail(usernameTextField.text!) {
+            errors = true
+            message += "Invalid Email Address"
+            alertWithTitle(title, message: message, ViewController: self, toFocus:self.usernameTextField)
+        } else if (passwordTextField.text?.count)! < 8 {
+            errors = true
+            message += "Password must be at least 8 characters"
+            alertWithTitle(title, message: message, ViewController: self, toFocus:self.passwordTextField)
+        }
+        return errors
+    }
+    
+    func isValidEmail(_ test:String) -> Bool {
+        // email validation here...
+        return true
+    }
+    
+    func alertWithTitle(_ title: String!, message: String, ViewController: UIViewController, handler: @escaping (UIAlertAction) -> (Void)) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel,handler: handler);
+        alert.addAction(action)
+        ViewController.present(alert, animated: true, completion:nil)
+    }
+    
+    func alertWithTitle(_ title: String!, message: String, ViewController: UIViewController,toFocus:UITextField) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel,handler: {_ in
+            toFocus.becomeFirstResponder()
+        });
+        alert.addAction(action)
+        ViewController.present(alert, animated: true, completion:nil)
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        self.activeTextField = textField
+        lastOffset = self.scrollView.contentOffset
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        activeTextField.resignFirstResponder()
+        activeTextField = nil
+        
+        if (textField == self.usernameTextField) {
+            self.passwordTextField.becomeFirstResponder()
+        } else {
+            let thereWereErrors = checkForErrors()
+            if !thereWereErrors {
+                loginButton.backgroundColor = UIColor.red
+            } else {
+                loginButton.backgroundColor = UIColor(red: 26.0/255.0, green: 188.0/255.0, blue: 156.0/255.0, alpha: 1.0)
+            }
+        }
+        
+        return true
     }
 }
 
 extension LoginViewController: WebSocketDelegate {
     func websocketDidConnect(socket: WebSocketClient) {
         print("Connected")
-        loginButton.isUserInteractionEnabled = true
+        loginButton.backgroundColor = UIColor(red: 26.0/255.0, green: 188.0/255.0, blue: 156.0/255.0, alpha: 1.0)
     }
     
     func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
@@ -66,37 +187,13 @@ extension LoginViewController: WebSocketDelegate {
     func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
         print(text)
         
-        let notificationView = UIView.init(frame: CGRect(x: 25, y: self.view.frame.height, width: self.loginButton.frame.width, height: self.loginButton.frame.height))
-        notificationView.backgroundColor = UIColor.darkGray
-        notificationView.cornerRadius = 5
-        notificationView.shadowColor = UIColor.init(red: 1.0,
-                                                    green: 1.0,
-                                                    blue: 1.0,
-                                                    alpha: 0.1)
+        self.loginActivityIndicator.stopAnimating()
+        self.loginActivityIndicator.isHidden = true
         
-        let rect = CGRect(x: notificationView.frame.midX,
-                          y: notificationView.frame.midY,
-                          width: self.loginButton.frame.width,
-                          height: self.loginButton.frame.height)
-        let notificationLabel = UILabel.init(frame: rect)
-        notificationLabel.text = "Login Succesful!"
-//        notificationLabel.font = UIFont.init(name: "MuktaVaani", size: 20.0)
-        notificationLabel.textColor = UIColor.white
-        
-        notificationView.addSubview(notificationLabel)
-        self.view.addSubview(notificationView)
-        
-        UIView.animate(withDuration: 0.5, animations: {
-            notificationView.frame = CGRect.init(x: 25, y: self.loginButton.frame.minY - 40, width: self.loginButton.frame.width, height: self.loginButton.frame.height)
-            Answers.logCustomEvent(withName: "LogInSuccessful", customAttributes: ["any":"something"])
-
-        }) { (bool) in
-            sleep(1)
+        Answers.logCustomEvent(withName: "LogInSuccessful", customAttributes: ["any":"something"])
+        alertWithTitle("Login Successful!", message: "", ViewController: self) { (alertAction) -> (Void) in
             self.performSegue(withIdentifier: "loginSuccessful", sender: self)
         }
-        
-        
-        
     }
     
     func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
